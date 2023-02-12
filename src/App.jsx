c6b8c73d31f0db1781ab9d3ce75cdbd966c2a81a
David-Koshkadze <prous3rr@pm.me>
2023-02-12 20:38:53 +0400

    changes 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,8 @@
 import { useMutltiStepForm } from "./useMultiStepForm";
 
 export default function App() {
-  const { steps, currentStepIndex, step, next, back } = useMutltiStepForm([
-    <div>First Step</div>,
-    <div>Second Step</div>,
-  ]);
 
   return (
-    <div
-      style={{
-        position: "relative",
-        background: "white",
-        border: "1px solid black",
-        padding: "2rem",
-        margin: "1rem",
-        borderRadius: "0.5rem",
-        fontFamily: "Arial"
-      }}
-    >
-      <form>
-        <div
-          style={{
-            position: "absolute",
-            top: ".5rem",
-            right: ".5rem"
-          }}
-        >
-          {currentStepIndex + 1} / {steps.length}
-        </div>
-        {step}
-
-        {currentStepIndex > 1 && <button onClick={back}>Back</button>}
-        <button onClick={next}>Next</button>
-      </form>
-    </div>
+    <div></div>
   );
 }
