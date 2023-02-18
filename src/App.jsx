9327dbbc385470f56173aca26ab948b2fd6a4641
David-Koshkadze <rootUS3rr@pm.me>
2023-02-18 11:23:33 +0400

    add design 1

--- src/App.jsx
+++ src/App.jsx
@@ -37,30 +37,51 @@ export default function App() {
   }
 
   return (
-    <div
-      className="relative bg-white border border-black rounded-lg p-8 m-4 w-max"
-    >
-      <form onSubmit={handleSubmit}>
-        <div className="absolute top-2 right-2">
-          {currentStepIndex + 1} / {steps.length}
+    <div className="w-full h-screen bg-light-gray grid place-items-center">
+      <div className="flex gap-8 bg-white p-4 shadow-sm rounded-2xl w-3/4">
+        <div className="rounded-2xl w-56 bg-green-500 pt-12 pl-8">
+          <div className="flex flex-col w-full gap-4 mx-auto">
+            {Array.from({ length: steps.length }, (_, index) => index + 1).map(
+              (stepItem, idx) => {
+                return (
+                  <div key={idx} className="flex w-full gap-3 items-center">
+                    <div className="w-8 h-8 rounded-full bg-teal-500 border border-white grid place-items-center">
+                      <span>{idx + 1}</span>
+                    </div>
+                    <div>
+                      <span>Step {idx + 1}</span>
+                      <div>Personal Info</div>
+                    </div>
+                  </div>
+                );
+              }
+            )}
+          </div>
         </div>
-        {step}
 
-        <div className="flex gap-4 mt-4 justify-end">
-          {!isFirstStep && (
-            <button
-              type="button"
-              onClick={back}
-              className="bg-green-500 py-2 px-4 rounded-lg"
-            >
-              Back
-            </button>
-          )}
-          <button type="submit" className="bg-green-500 py-2 px-4 rounded-lg">
-            {!isLastStep ? "Next" : "Finish"}
-          </button>
+        <div>
+          <form onSubmit={handleSubmit}>
+            {step}
+            <div className="flex gap-4 mt-4 justify-end">
+              {!isFirstStep && (
+                <button
+                  type="button"
+                  onClick={back}
+                  className="bg-green-500 py-2 px-4 rounded-lg"
+                >
+                  Back
+                </button>
+              )}
+              <button
+                type="submit"
+                className="bg-green-500 py-2 px-4 rounded-lg"
+              >
+                {!isLastStep ? "Next" : "Finish"}
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
